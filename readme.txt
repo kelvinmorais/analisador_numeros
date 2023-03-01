17509f113faff3f3c0fecdc477119cb65f07a8de
Kelvin Morais <kelvintitomorais@hotmail.com>
2023-03-01 16:50:05 -0300

    OK

--- readme.txt
+++ readme.txt
@@ -13,11 +13,14 @@ Funcionalidades
 	Adicionar o valor digitado no campo de número na caixa			OK
 		Se não tiver valor digitado, apresentar erro (extra)			OK
 		se o valor digitado naõ estiver de 1 a 100, apresentar erro		OK
-		Se o valor já foi digitado, deve apresentar erro			OK			Se pressionar o botão reset, zerar todos dadso (extra)		OK	
+		Se o valor já foi digitado, deve apresentar erro			OK			
+		Se pressionar o botão reset, zerar todos dadso (extra)		OK	
 		
-	ao pressionar botão finalizar, apresentar:
-		contador de números
-		maior valor
-		menor valor
-		soma dos valores
-		média
+	ao pressionar botão finalizar, 
+		se ainda não tiver sido feito nenhuma finalização(extra)			OK
+			apresentar:										OK
+				contador de números							OK
+				maior valor									OK
+				menor valor									OK
+				soma dos valores								OK
+				média										OK
--- script.js
+++ script.js
@@ -5,7 +5,8 @@ var botaoadd = window.document.querySelector("input#add"),
 botaoadd.addEventListener('click', clickadd)
 botaof.addEventListener('click', clickfim)
 resetdom.addEventListener('click', reset)
-var caixa = []
+var caixa = [],
+    finish = 0
 
 function clickadd(){
     let numdom = window.document.querySelector("input#num")    
@@ -42,10 +43,41 @@ function numduplicado(num){
 }
 
 function clickfim(){
-    window.alert('Ola3')
+    alert(finish)
+   if(finish > 0){
+        alert('Reset para poder analisar novos números')
+    } else if (finish == 0) {
+        caixa.sort() //ordenador
+    let qtdnum = caixa.length, //contador de número
+        maiorn = caixa[qtdnum-1], //maior valor
+        menorn = caixa[0], //menor valor
+        //soma = soma(caixa) //soma
+        media = soma(caixa) / qtdnum,
+        res1 = document.querySelector("p#res-qtd"),
+        res2 = document.querySelector("p#res-maior"),
+        res3 = document.querySelector("p#res-menor"),
+        res4 = document.querySelector("p#res-soma"),
+        res5 = document.querySelector("p#res-media")
+
+    res1.innerHTML = `Você digitou ${qtdnum} número \n`
+    res2.innerHTML = `O maior valor digitado, foi o número ${maiorn} \n`
+    res3.innerHTML = `O menor valor digitado, foi o número ${menorn} \n`
+    res4.innerHTML = `A soma dos valores digitados é : ${soma(caixa)} \n`
+    res5.innerHTML = `A média dos valores digitados é: ${media} \n`
+    finish++
+    }
+    
 }
 
 function reset(){
     caixadom.innerHTML = ''
     caixa = []
+    finish = 0
+}
+
+function soma(caixa){
+    var soma = 0
+    for(let ini in caixa){
+        soma+=caixa[ini]
+    } return soma
 }
